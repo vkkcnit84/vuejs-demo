e8be760ff9d5f4ef8a2a023ef4b49a6e70f28f4e
vinay-st <vkkcnit@gmail.com>
2025-03-30 17:52:09 +0530

    fix issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import DashboardView from '@/views/DashboardView.vue'
-// import InfiniteScrollView from '@/views/InfiniteScrollView.vue'
-// import SearchView from '@/views/SearchView.vue'
 
 const routes = [
   { path: '/', component: DashboardView },
@@ -10,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // 🔥 Change this to hash mode
   routes,
 })
 
